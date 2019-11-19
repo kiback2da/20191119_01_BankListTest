3dc46aa9b454593c9490cea2de8bced5ea22b2b0
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-19 20:07:19 +0900

    은행목록 가여오는 API 호출 메소드 생성

--- app/src/main/java/com/tjoeun/a20191119_01_banklisttest/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191119_01_banklisttest/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.tjoeun.a20191119_01_banklisttest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.tjoeun.a20191114_01_okhttp.utils.ServerUtil
 import com.tjoeun.a20191119_01_banklisttest.adapter.BankListAdapter
 import com.tjoeun.a20191119_01_banklisttest.data.BankData
-import okhttp3.*
 import org.json.JSONObject
-import java.io.IOException
 
 class MainActivity : BaseActivity() {
 
@@ -28,27 +25,17 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        getBanksFromServer()
     }
 
     fun getBanksFromServer(){
-        var client = OkHttpClient()
-        var urlBuilder = HttpUrl.parse("${BASE_URL}/info/bank")!!.newBuilder()
+        ServerUtil.getRequestBankInfo(mContext,object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(json: JSONObject) {
 
-        //URL 최종 확정
-        var requestUrl = urlBuilder.build().toString()
+                var data = json.getJSONObject("data")
+                var bankArray = data.getJSONArray("banks")
+                Log.d("로그 : bankArray","${bankArray.length()}")
 
-        var request = Request.Builder().url(requestUrl).build()
-
-        client.newCall(request).enqueue(object : Callback{
-            override fun onFailure(call: Call, e: IOException) {
-                Log.e("로그 : 서버통신에러",e.localizedMessage)
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                Log.e("로그 : onResponse",response.toString())
-                val json = JSONObject(response.body().toString())
-                handler?.onResponse(json)
             }
 
         })
--- app/src/main/java/com/tjoeun/a20191119_01_banklisttest/utils/ServerUtil.kt
+++ app/src/main/java/com/tjoeun/a20191119_01_banklisttest/utils/ServerUtil.kt
@@ -16,52 +16,26 @@ class ServerUtil {
         //어느 서버에 접속할지 서버 주소를 저장하는 변수
         var BASE_URL = "http://192.168.0.26:5000"
 
-        fun postRequestLogin(context: Context, loginId:String, loginPassword:String, handler:JsonResponseHandler?){
-
-            //우리가 만드는 안드로이드 앱을 클라이어트 역할로 동작하게 해주는 클래스
-            var client = OkHttpClient()
-
-            //기능 주소와 서버주소를 조합해서 실제 요청 주소 완성
-            var url = "${BASE_URL}/auth"
-
-            //POST 메소드에서 요구하는 파라미터를 formBody에 기록
-            var formBody = FormBody.Builder()
-                             .add("login_id",loginId)
-                             .add("password",loginPassword)
-                             .build()
-
-            //실제로 날아갈 요청(request) 생성
-            var request = Request.Builder().url(url).post(formBody).build()
-
-            client.newCall(request).enqueue(object : Callback{
-                override fun onFailure(call: Call, e: IOException) {
-                    Log.e("서버통신에러",e.localizedMessage)
-                }
-
-                override fun onResponse(call: Call, response: Response) {
-                    var body = response.body()!!.string()
-                    Log.d("서버",body)
-
-                    var json = JSONObject(body)
-                    handler?.onResponse(json)
-                }
-
-            })
+        interface JsonResponseHandler{
+            fun onResponse(json : JSONObject)
         }
 
-        fun putRequestSignUp(context: Context, loginId: String, loginPassword: String, name: String, phoneNum: String, handler: JsonResponseHandler?){
+        fun getRequestBankInfo(context: Context, handler: ServerUtil.JsonResponseHandler){
             var client = OkHttpClient()
-            var url = "${BASE_URL}/auth"
-            var formBody = FormBody.Builder().add("login_id",loginId).add("password",loginPassword).add("name",name).add("phone",phoneNum).build()
-            var request = Request.Builder().url(url).post(formBody).build()
+            var urlBuilder = HttpUrl.parse("${BASE_URL}/info/bank")!!.newBuilder()
+            //파라미터 첨부가 필요없다.
+            var requestUrl = urlBuilder.build().toString()
+            var request = Request.Builder().url(requestUrl).build()
+
             client.newCall(request).enqueue(object : Callback{
                 override fun onFailure(call: Call, e: IOException) {
                     Log.d("로그","서버 통신 에러")
                 }
 
                 override fun onResponse(call: Call, response: Response) {
-                    var json = JSONObject(response.body().toString())
-                    handler?.onResponse(json)
+                    var body = response.body()!!.toString()
+                    val json = JSONObject(body)
+                    handler.onResponse(json)
                 }
 
             })
