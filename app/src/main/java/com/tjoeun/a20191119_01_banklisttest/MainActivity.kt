83683cca1eec9f312020947e0de87a4e1522c9fd
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-19 19:54:39 +0900

    은행 목록 받아올 기초 준비 작업   - 액티비티에 리스트 생성 / 함수 생성   - okhttp 라이브러리 설치   - 권한 설정(INTERNET), 평문통신 허용

--- app/src/main/java/com/tjoeun/a20191119_01_banklisttest/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191119_01_banklisttest/MainActivity.kt
@@ -2,8 +2,20 @@ package com.tjoeun.a20191119_01_banklisttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.tjoeun.a20191114_01_okhttp.utils.ServerUtil
+import com.tjoeun.a20191119_01_banklisttest.adapter.BankListAdapter
+import com.tjoeun.a20191119_01_banklisttest.data.BankData
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : BaseActivity() {
+
+    var bankList = ArrayList<BankData>()
+    var bankAdapter : BankListAdapter? = null
+    var BASE_URL = "http://192.168.0.26:5000"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,8 +24,33 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
     }
 
     override fun setValues() {
+
+    }
+
+    fun getBanksFromServer(){
+        var client = OkHttpClient()
+        var urlBuilder = HttpUrl.parse("${BASE_URL}/info/bank")!!.newBuilder()
+
+        //URL 최종 확정
+        var requestUrl = urlBuilder.build().toString()
+
+        var request = Request.Builder().url(requestUrl).build()
+
+        client.newCall(request).enqueue(object : Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("로그 : 서버통신에러",e.localizedMessage)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                Log.e("로그 : onResponse",response.toString())
+                val json = JSONObject(response.body().toString())
+                handler?.onResponse(json)
+            }
+
+        })
     }
 }
--- app/src/main/java/com/tjoeun/a20191119_01_banklisttest/adapter/BankListAdapter.kt
+++ app/src/main/java/com/tjoeun/a20191119_01_banklisttest/adapter/BankListAdapter.kt
@@ -1,8 +1,14 @@
 package com.tjoeun.a20191119_01_banklisttest.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.widget.ArrayAdapter
 import com.tjoeun.a20191119_01_banklisttest.data.BankData
 
 class BankListAdapter(context: Context, res:Int, list:ArrayList<BankData>) : ArrayAdapter<BankData>(context, res, list){
+
+    var mContext = context
+    var mList = list
+    var inf = LayoutInflater.from(mContext)
+
 }
